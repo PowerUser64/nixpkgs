7fde212a8be75615942d57022d5c85a0ec07a7fd
PowerUser64 <blake@blakenorth.net>
2024-10-16 02:21:09 -0700

    gef: add option to enable gef extras

--- pkgs/by-name/ge/gef/package.nix
+++ pkgs/by-name/ge/gef/package.nix
@@ -4,6 +4,7 @@
 , makeWrapper
 , gdb
 , gdb-binary ? "${lib.getExe gdb}"
+, enableGefExtras ? false
 , python3
 , bintools-unwrapped
 , file
@@ -13,16 +14,40 @@
 }:
 
 let
-  pythonPath = with python3.pkgs; makePythonPath [
+  pythonDeps = with python3.pkgs; [
     keystone-engine
     unicorn
     capstone
     ropper
+  ] ++ lib.optionals enableGefExtras [
+    pygments
+    requests
+    rpyc
   ];
 
-in stdenv.mkDerivation rec {
+  pythonPath = with python3.pkgs; makePythonPath pythonDeps;
+
+  gefVersion = "2024.06";
+
+  gef-extras = stdenv.mkDerivation rec {
+    name = "gef-extras";
+    version = gefVersion;
+    src = fetchFromGitHub {
+      owner = "hugsy";
+      repo = name;
+      rev = "2025.01";
+      hash = "sha256-ZBn8sOAR2togY90MzvwZNKtZDT3WmESrfI9mjOfyUM0=";
+    };
+    installPhase = ''
+      set -x
+      cp -r . $out
+    '';
+  };
+
+in
+stdenv.mkDerivation rec {
   pname = "gef";
-  version = "2024.06";
+  version = gefVersion;
 
   src = fetchFromGitHub {
     owner = "hugsy";
@@ -35,19 +60,31 @@ in stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ makeWrapper ];
 
-  installPhase = ''
-    mkdir -p $out/share/gef
-    cp gef.py $out/share/gef
-    makeWrapper ${gdb-binary} $out/bin/gef \
+  installPhase =
+    # bash
+    ''
+      mkdir -p $out/share/gef
+      cp gef.py $out/share/gef
+      makeWrapper ${gdb-binary} $out/bin/gef \
       --add-flags "-q -x $out/share/gef/gef.py" \
       --set NIX_PYTHONPATH ${pythonPath} \
       --prefix PATH : ${lib.makeBinPath [
-        python3
-        bintools-unwrapped # for readelf
-        file
-        ps
-      ]}
-  '';
+      python3
+      bintools-unwrapped # for readelf
+      file
+      ps
+      ]} '' + lib.optionalString enableGefExtras
+      # Add flags from the gef-extras installer script
+      # https://github.com/hugsy/gef/blob/f9f80450dc17ffd573362b73fdff0a71044de503/scripts/gef-extras.sh#L41-L48
+      # bash
+      ''  --add-flags -ex --add-flags "'pi gef.config[\"context.layout\"] += \" syscall_args\"'" \
+          --add-flags -ex --add-flags "'pi gef.config[\"context.layout\"] += \" libc_function_args\"'" \
+          --add-flags -ex --add-flags "'gef config gef.extra_plugins_dir ${gef-extras}/scripts'" \
+          --add-flags -ex --add-flags "'gef config pcustom.struct_path ${gef-extras}/structs'" \
+          --add-flags -ex --add-flags "'gef config syscall-args.path ${gef-extras}/syscall-tables'" \
+          --add-flags -ex --add-flags "'gef config context.libc_args True'" \
+          --add-flags -ex --add-flags "'gef config context.libc_args_path \"${gef-extras}/glibc-function-args\"'"
+      '';
 
   nativeCheckInputs = [
     gdb
@@ -55,13 +92,8 @@ in stdenv.mkDerivation rec {
     ps
     git
     python3
-    python3.pkgs.pytest
-    python3.pkgs.pytest-xdist
-    python3.pkgs.keystone-engine
-    python3.pkgs.unicorn
-    python3.pkgs.capstone
-    python3.pkgs.ropper
-  ];
+  ] ++ pythonDeps;
+
   checkPhase = ''
     # Skip some tests that require network access.
     sed -i '/def test_cmd_shellcode_get(self):/i \ \ \ \ @unittest.skip(reason="not available in sandbox")' tests/runtests.py
